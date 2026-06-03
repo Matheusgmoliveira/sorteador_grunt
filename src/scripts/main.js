3403475b189bbd0c622c8e3878c376f9734f6d6a
Matheus Gonçalves <124847215+Matheusgmoliveira@users.noreply.github.com>
2026-06-03 15:03:32 -0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('form-sorteador').addEventListener('submit', function(evento){
         evento.preventDefault();
-        let numeroMaximo = document.getElementById('numero-maximo').value;
+        let numeroMaximo=6;
+        //let numeroMaximo = document.getElementById('numero-maximo').value;
         numeroMaximo = parseInt(numeroMaximo);
 
         let numeroAleatorio = Math.random() * numeroMaximo;
