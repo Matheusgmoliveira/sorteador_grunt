5837ff61d1798026820da0dcf36062264a6aeb7d
Matheus Gonçalves <124847215+Matheusgmoliveira@users.noreply.github.com>
2026-06-03 15:34:33 -0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
+
     document.getElementById('form-sorteador').addEventListener('submit', function (evento) {
         evento.preventDefault();
 
-        let numeroMaximo = 6;
-        numeroMaximo = parseInt(numeroMaximo);
+        const numeroMaximo = 6;
+        const numeroAleatorio = Math.floor(Math.random() * numeroMaximo) + 1;
 
-        let numeroAleatorio = Math.random() * numeroMaximo;
-        numeroAleatorio = Math.floor(numeroAleatorio + 1);
+        const resultado = document.getElementById('resultado-valor');
+        const boxResultado = document.querySelector('.resultado');
 
-        document.getElementById('resultado-valor').innerText = numeroAleatorio;
-        document.querySelector('.resultado').style.display = 'block';
+        boxResultado.style.display = 'block';
 
         if (numeroAleatorio <= 4) {
-            document.getElementById('resultado-valor').innerText = "Parabéns, retire seu brinde!";
-        }else{
-           document.getElementById('resultado-valor').innerText = "Não foi dessa vez, Tente novamente!"; 
+            resultado.innerText = "Parabéns, retire seu brinde!";
+        } else {
+            resultado.innerText = "Não foi dessa vez, tente novamente!";
         }
     });
+
+    document.getElementById('btn-limpar').addEventListener('click', function () {
+        document.getElementById('resultado-valor').innerText = '';
+        document.querySelector('.resultado').style.display = 'none';
+    });
+
 });
