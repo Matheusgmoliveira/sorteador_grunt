231659b25f4398a123f3ecd58e2f56e716e3866b
Matheus Gonçalves <124847215+Matheusgmoliveira@users.noreply.github.com>
2026-06-03 15:21:10 -0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,18 @@
-document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById('form-sorteador').addEventListener('submit', function(evento){
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('form-sorteador').addEventListener('submit', function (evento) {
         evento.preventDefault();
-        let numeroMaximo=6;
-        //let numeroMaximo = document.getElementById('numero-maximo').value;
+
+        let numeroMaximo = 6;
         numeroMaximo = parseInt(numeroMaximo);
 
         let numeroAleatorio = Math.random() * numeroMaximo;
-        numeroAleatorio = Math.floor(numeroAleatorio + 1)
+        numeroAleatorio = Math.floor(numeroAleatorio + 1);
+
         document.getElementById('resultado-valor').innerText = numeroAleatorio;
-        document.querySelector('.resultado').style.display= 'block';
-    })
-        
-})
+        document.querySelector('.resultado').style.display = 'block';
+
+        if (numeroAleatorio <= 4) {
+            document.getElementById('resultado-valor').innerText = "Teste";
+        }
+    });
+});
